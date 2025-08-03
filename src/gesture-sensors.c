1223695320dbcd9211fbb483bca9c7c0bdc40666
Bardia Moshiri <bardia@furilabs.com>
2025-08-02 22:00:55 -0400

    gesture-sensors: make read and write functions 4 indent
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- src/gesture-sensors.c
+++ src/gesture-sensors.c
@@ -40,36 +40,36 @@ void
 write_to_file(const char *path,
               const char *value)
 {
-  g_debug("Attempting to write to %s: %s\n", path, value);
+    g_debug("Attempting to write to %s: %s\n", path, value);
 
-  int fd = open(path, O_WRONLY);
-  if (fd == -1) {
-    perror("open");
-    return;
-  }
+    int fd = open(path, O_WRONLY);
+    if (fd == -1) {
+        perror("open");
+        return;
+    }
 
-  if (write(fd, value, strlen(value)) == -1)
-    perror("write");
+    if (write(fd, value, strlen(value)) == -1)
+        perror("write");
 
-  close(fd);
+    close(fd);
 }
 
 char *
 read_from_file(const char *path)
 {
-  FILE *file = fopen(path, "r");
-  if (!file)
-    return NULL;
+    FILE *file = fopen(path, "r");
+    if (!file)
+        return NULL;
 
-  char buffer[256];
-  if (!fgets(buffer, sizeof(buffer), file)) {
+    char buffer[256];
+    if (!fgets(buffer, sizeof(buffer), file)) {
+        fclose(file);
+        return NULL;
+    }
+
     fclose(file);
-    return NULL;
-  }
-
-  fclose(file);
-  buffer[strcspn (buffer, "\n")] = '\0';
-  return strdup(buffer);
+    buffer[strcspn (buffer, "\n")] = '\0';
+    return strdup(buffer);
 }
 
 void
